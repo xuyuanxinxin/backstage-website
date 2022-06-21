112e8e7a9a957a84378ce30b2b2c93dbb0cd06b2
xuyuanxinxin <1846234482@qq.com>
2022-06-21 23:45:28 +0800

    remove useless dependencies & add vicons

--- src/components/Toolbar.vue
+++ src/components/Toolbar.vue
@@ -4,6 +4,12 @@
       <n-input round size="small" placeholder="输入要搜索的内容" />
     </div>
     <div class="nav-item end-group">
+      <n-button text @click="fullscreen">
+        <n-icon size="20">
+          <contract v-if="isFullScreen" />
+          <expand v-else />
+        </n-icon>
+      </n-button>
       <n-avatar
         :size="30"
         round
@@ -14,7 +20,21 @@
 </template>
 
 <script setup lang="ts">
-import { NInput, NAvatar } from 'naive-ui'
+import { NInput, NAvatar, NButton, NIcon } from 'naive-ui'
+import { Expand, Contract } from '@vicons/ionicons5'
+import { ref } from 'vue'
+const fullscreen = () => {
+  if (!document.fullscreenElement) {
+    isFullScreen.value = true
+    document.documentElement.requestFullscreen()
+  } else {
+    isFullScreen.value = false
+    if (document.exitFullscreen) {
+      document.exitFullscreen()
+    }
+  }
+}
+const isFullScreen = ref(false)
 </script>
 
 <style scoped lang="scss">
@@ -22,6 +42,8 @@ import { NInput, NAvatar } from 'naive-ui'
   display: inline-block;
 }
 .end-group {
+  display: flex;
+  align-items: center;
   margin: 0 15px 0 auto;
 }
 nav {
